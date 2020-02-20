55bd3c7df3b2f93f989e15ac6650003077159412
Jason Haugland <jasonhaugland@Jasons-MacBook-Pro-2.local>
2020-02-20 17:32:06 -0600

    add transaction indexing

--- scripts/genData.sh
+++
@@ -1,1 +0,0 @@
-curl -i -H "noOfDays=5" -H "noOfTHreads=3" -H "createStr=create" -H "noOfCustomersStr='100'" -H "noOfTransactionsStr=500" http://localhost:8080/generateData
---
+++ scripts/getByMerchant.sh
@@ -0,0 +1,1 @@
+curl -X GET -H "Content-Type: application/json"  'http://localhost:8080/categgorydescrip?merchantcateg=5435&lastname=00097'
