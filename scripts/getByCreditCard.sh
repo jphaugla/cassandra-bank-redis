2b598cb3d09ea9947e99621818a169ecd049df55
Jason Haugland <jasonhaugland@Jasons-MacBook-Pro-2.local>
2020-02-21 16:15:23 -0600

    add calling API scripts

---
+++ scripts/getByCreditCard.sh
@@ -0,0 +1,1 @@
+curl -X GET -H "Content-Type: application/json"  'http://localhost:8080/creditCardTransactions/?creditCard=2529f84bxececx4fc9xb81dx35742150b0e4&account=Acct2&from=02/14/2020&to=02/25/2020'
---
+++ scripts/putTag.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -d '{ "accountNo": "Acct2", "trandate": "02/18/2020", "transactionID": "148", "tag": "Haugland" }' http://localhost:8080/posttag
